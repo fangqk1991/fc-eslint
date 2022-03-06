c60847fdd0b7b91a433922ff4a37f073c2175685
fangqk1991 <me@fangqk.com>
2022-03-06 18:13:44 +0800

    v0.0.1 - fc-eslint init

---
+++ src/eslintrc-backend.js
@@ -0,0 +1,24 @@
+const rules = require('./rules')
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: null,
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  rules,
+}
